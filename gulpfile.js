5422642b9644ea43ef11a750a0ce0054bae70528
David Lin <davidlin@qnap.com>
2017-08-28 17:34:41 +0800

    add layouts

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,13 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    //mix.sass('app.scss');
+    mix.copy('bower_components/bootstrap/dist/fonts', 'public/assets/fonts');
+    mix.styles([
+        'bower_components/bootstrap/dist/css/bootstrap.css'
+    ], 'public/assets/stylesheets/styles.css', './');
+    mix.scripts([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+    ], 'public/assets/scripts/frontend.js', './');
 });
