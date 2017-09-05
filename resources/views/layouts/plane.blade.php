ba2262b8f6494b084a20d0ac7f0be8068959ffe2
David Lin <davidlin@qnap.com>
2017-09-05 17:23:22 +0800

    fix google analytics

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -14,13 +14,15 @@
 
   <link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
   <!-- Global Site Tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105848006-1"></script>
   <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};
-    gtag('js', new Date());
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    gtag('config', 'UA-105848006-1');
+    ga('create', 'UA-105848006-1', 'auto');
+    ga('send', 'pageview');
+
   </script>
 </head>
 <body>
